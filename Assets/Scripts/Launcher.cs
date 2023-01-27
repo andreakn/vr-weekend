de6026d0771493aceddd61f845d6746d8e0246c9
Andreas Knudsen <andreas.knudsen@gmail.com>
2023-01-27 11:49:08 +0100

    initial

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+
+    public PhotonView playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //try to connect
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room success");
+        PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+    }
+}
