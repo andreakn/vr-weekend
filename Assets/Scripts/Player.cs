882303806bd64c921b683ca30cd8797602c2d040
Joakim Stien <joakim.stien@icloud.com>
2023-01-27 17:36:08 +0100

    Oops

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,22 +7,18 @@ using Photon.Pun;
 public class Player : MonoBehaviourPunCallbacks
 {
     public string playerName = "Gandalf the Vague";
-    private int score = 0;
-
-private Collider collider;
+    private Collider collider;
 
     float turnSpeed = 4.0f;
     Rigidbody rigidbody = null;
     GameObject cam = null;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         collider = GetComponent<Collider>();
     }
-
-
-  
 
     void FixedUpdate()
     {
@@ -37,19 +33,8 @@ private Collider collider;
         float mouseX = Input.GetAxis("Mouse X");
         transform.Rotate(new Vector3(0,mouseX*turnSpeed,0));
         float mouseY = Input.GetAxis("Mouse Y");
-
-        var scoreboard = GameController.scoreboard;
-        if (scoreboard == null) {
-            Debug.Log("scoreboard is null");
-        }
-        if (scoreboard.photonView == null) {
-            Debug.Log("oh shiiiiiiiiiiiiiiiiiiiiiiiiII");
-        }
-
-
-
-        GameController.scoreboard.photonView.RPC("OnPlayerScored", RpcTarget.All, photonView.Owner.ActorNumber, score);
     }
+
      private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name.Contains("leDwarf")){
