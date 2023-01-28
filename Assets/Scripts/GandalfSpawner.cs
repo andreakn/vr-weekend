4eaa1991452e38f07294ef33d0aa191468ca3125
Joakim Stien <joakim.stien@icloud.com>
2023-01-28 12:15:55 +0100

    maoiooiu

--- Assets/Scripts/GandalfSpawner.cs
+++ Assets/Scripts/GandalfSpawner.cs
@@ -5,12 +5,20 @@ using Photon.Pun;
 
 public class GandalfSpawner : MonoBehaviourPunCallbacks {
     public GameObject gandalfPrefab;
+    private GameObject gandalf;
 
     void Start() {
         if (!photonView.IsMine) {
-            var gandalf = MonoBehaviour.Instantiate(gandalfPrefab);
-            gandalf.transform.parent = transform;
+            gandalf = MonoBehaviour.Instantiate(gandalfPrefab, Vector3.zero, Quaternion.Euler(270, 0, 0));
         }
         
+    }
+
+    void Update() {
+        if (gandalf != null) {
+            gandalf.transform.position = transform.position;
+            var rotation = transform.rotation;
+            gandalf.transform.rotation = Quaternion.Euler(270, rotation.eulerAngles.y - 180, 0);
+        }
     }
 }
